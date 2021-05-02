b6b9eb71c49bd18b671a95ef88e648107efe9560
cibikumar <cibikumarv@gmail.com>
2021-05-02 13:26:18 +0530

    Added a drawer and partly implemented it.

--- lib/Screens/tabs_screen.dart
+++ lib/Screens/tabs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/widgets/main_drawer.dart';
 import 'categories_screen.dart';
 import 'favourites_screen.dart';
 
@@ -32,6 +33,9 @@ class _TabScreenState extends State<TabsScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(_pages[_selectedPageIndex]["title"]),
+      ),
+      drawer: Drawer(
+        child: MainDrawer(),
       ),
       body: _pages[_selectedPageIndex]["page"],
       bottomNavigationBar: BottomNavigationBar(
